d7d07defe81bb8a6ee31b7ec6d44e066bce9bdd6
Andrade-Miranda <gxandrade.miranda@gmail.com>
2024-09-16 17:22:52 +0100

    add parsing variables

--- DL_code/train.py
+++ DL_code/train.py
@@ -1,7 +1,8 @@
 #Setup Imports
 import os
 import json
-import tempfile
+import argparse
+
 import time
 import random
 from monai.utils import set_determinism
@@ -34,10 +35,6 @@ import wandb
 
 print_config()
 
-#Setup Directory
-directory = 'DL_code'
-root_dir = tempfile.mkdtemp() if directory is None else directory
-print(root_dir)
 
 
 ## Setup average meter, fold reader, checkpoint saver
@@ -58,7 +55,7 @@ class AverageMeter(object):
         self.avg = np.where(self.count > 0, self.sum / self.count, self.sum)
 
 def restore_Model(file,model,opt):
-    checkpoint = torch.load(file,map_location=opt['device'],weights_only=True)
+    checkpoint = torch.load(file,map_location=opt.device,weights_only=True)
     if 'state_dict' in checkpoint:
         model.load_state_dict(
             checkpoint['state_dict'],strict=False)
@@ -85,13 +82,13 @@ def datafold_read(datalist):
 def save_checkpoint(model, epoch,opt, filename="model.pt", best_acc=0):
     state_dict = model.state_dict()
     save_dict = {"epoch": epoch, "best_acc": best_acc, "model_state_dict": state_dict}
-    pathmodel = os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun'],filename)
+    pathmodel = os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun,filename)
     torch.save(save_dict, pathmodel)
     print("Saving checkpoint", filename)
    
  
 ## Setup dataloader ####################################################################################################################   
-def get_loader(batch_size, json_list, roi):  
+def get_loader(batch_size, json_list, roi,MoreDA):  
     datalist_json = json_list
     train_files, validation_files,test_files = datafold_read(datalist=datalist_json)
   
@@ -103,18 +100,22 @@ def get_loader(batch_size, json_list, roi):
             transforms.ScaleIntensityRangePercentilesd(keys=["image"], lower=1, upper=99, b_min=-1,b_max=1, clip=True,dtype=np.float32),
             transforms.Resized(keys=["image"], spatial_size=(roi[0], roi[1]), mode="bilinear"),
             transforms.Resized(keys=["label"], spatial_size=(roi[0], roi[1]), mode="nearest-exact"),
+        ])
+    if MoreDA:
+        extra_transform= transforms.Compose([
+                transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+                transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+                transforms.RandRotated(keys=["image", "label"],mode=("bilinear", "nearest"),range_x=np.pi/6,range_y=np.pi/6),
 
-            #transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
-            #transforms.RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
-            #transforms.RandRotated(keys=["image", "label"],mode=("bilinear", "nearest"),range_x=np.pi/6,range_y=np.pi/6),
-
-            #transforms.RandAffined(keys=["image", "label"],prob=0.7,shear_range=(0.5,0.5),mode=['bilinear','nearest'],padding_mode='zeros'),            
-            #transforms.RandAdjustContrastd(keys=["image"],prob=1,gamma=(0.8,1.2)),
+                transforms.RandAffined(keys=["image", "label"],prob=0.7,shear_range=(0.5,0.5),mode=['bilinear','nearest'],padding_mode='zeros'),            
+                transforms.RandAdjustContrastd(keys=["image"],prob=1,gamma=(0.8,1.2)),
             
-            #transforms.RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
-            #transforms.RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
-        ]
-    )
+                transforms.RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
+                transforms.RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
+            ])
+        train_transform=transforms.Compose(train_transform,extra_transform)
+    
+    
     val_transform = transforms.Compose(
         [
             transforms.LoadImaged(keys=["image"],reader=PILReader(reverse_indexing=False,converter=lambda image: image.convert("RGB")),dtype='float',image_only=False),
@@ -159,12 +160,12 @@ def get_loader(batch_size, json_list, roi):
 
 
 ## Define Train  ####################################################################################################
-def train_epoch(model, loader, optimizer, epoch, loss_func,wandb_logger,debug,dice_Train):
+def train_epoch(model, loader, optimizer, epoch, loss_func,wandb_logger,dice_Train,opt):
     model.train()
     start_time = time.time()
     run_loss = AverageMeter()
     for idx, batch_data in enumerate(loader):
-        data, target = batch_data["image"].to(device), batch_data["label"].to(device)
+        data, target = batch_data["image"].to(opt.device), batch_data["label"].to(opt.device)
         logits = model(data.float())
 
         val_labels_list = decollate_batch(target)
@@ -175,9 +176,9 @@ def train_epoch(model, loader, optimizer, epoch, loss_func,wandb_logger,debug,di
         loss = loss_func(logits, target)
         loss.backward()
         optimizer.step()
-        run_loss.update(loss.item(), n=batch_size)
+        run_loss.update(loss.item(), n=opt.batch_size)
         print(
-            "Epoch {}/{} {}/{}".format(epoch, max_epochs, idx, len(loader)),
+            "Epoch {}/{} {}/{}".format(epoch, opt.max_epochs, idx, len(loader)),
             "loss: {:.4f}".format(run_loss.avg),
             "time {:.2f}s".format(time.time() - start_time),
         )
@@ -195,13 +196,13 @@ def train_epoch(model, loader, optimizer, epoch, loss_func,wandb_logger,debug,di
     return run_loss.avg
 
 ## Define val  ####################################################################################################
-def val_epoch(model, loader, epoch, acc_func, model_inferer=None, post_trans=None,wandb_logger=None):
+def val_epoch(model, loader, epoch, acc_func, model_inferer=None, post_trans=None,wandb_logger=None,opt=None):
     model.eval()
     start_time = time.time()
 
     with torch.no_grad():
         for idx, batch_data in enumerate(loader):
-            data, target = batch_data["image"].to(device), batch_data["label"].to(device)
+            data, target = batch_data["image"].to(opt.device), batch_data["label"].to(opt.device)
             logits = model_inferer(data.float(),model)
             val_labels_list = decollate_batch(target)
             val_outputs_list = decollate_batch(logits)
@@ -209,7 +210,7 @@ def val_epoch(model, loader, epoch, acc_func, model_inferer=None, post_trans=Non
 
             acc_func(y_pred=val_output_convert, y=val_labels_list)
 
-            if  debug:
+            if  opt.debug:
                 # Create a figure and subplots
                 fig, axs = plt.subplots(2, 2, figsize=(10, 10))
                 # Plot each image in a different subplot
@@ -228,13 +229,13 @@ def val_epoch(model, loader, epoch, acc_func, model_inferer=None, post_trans=Non
                 plt.tight_layout()
                 # Add a common title for all subplots
                 fig.suptitle(batch_data['image_meta_dict']['filename_or_obj'][0].split('/')[-1], fontsize=16)
-                plt.show()
-                plt.savefig(os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun'],str(idx)+'.pdf'))
+                #plt.show()
+                plt.savefig(os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun,str(idx)+'.pdf'))
             
 
         average_dice = acc_func.aggregate()[0].item()    
         print(
-                "Val {}/{} {}/{}".format(epoch, max_epochs, idx, len(loader)),
+                "Val {}/{} {}/{}".format(epoch, opt.max_epochs, idx, len(loader)),
                 ", Avg Dice Coeff:", average_dice,
                 ", time {:.2f}s".format(time.time() - start_time),
             )
@@ -249,7 +250,7 @@ def val_epoch(model, loader, epoch, acc_func, model_inferer=None, post_trans=Non
 ## Define test  ####################################################################################################
 def test_data(model, loader, acc_func, model_inferer=None, post_trans=None,wandb_logger=None):
     
-    pathmodel = os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun'],'model.pt')
+    pathmodel = os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun,'model.pt')
     model=restore_Model(pathmodel,model,opt)
     model.eval()
     with torch.no_grad():
@@ -261,7 +262,7 @@ def test_data(model, loader, acc_func, model_inferer=None, post_trans=None,wandb
             val_output_convert = [post_trans(val_pred_tensor) for val_pred_tensor in val_outputs_list]
 
             acc_func(y_pred=val_output_convert, y=val_labels_list)
-            filename=os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun'],batch_data['image_meta_dict']['filename_or_obj'][0].split('/')[-1].split('.')[0]+'.png')
+            filename=os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun,batch_data['image_meta_dict']['filename_or_obj'][0].split('/')[-1].split('.')[0]+'.png')
 
             im = Image.fromarray(val_output_convert[0][0].detach().cpu().numpy().astype(np.uint8)*255)
             im.save(filename)
@@ -303,7 +304,7 @@ def trainer(
     loss_epochs = []
     trains_epoch = []
     
-    for epoch in range(start_epoch, max_epochs):
+    for epoch in range(start_epoch, opt.max_epochs):
         print(time.ctime(), "Epoch:", epoch)
         epoch_time = time.time()
         
@@ -313,10 +314,10 @@ def trainer(
             optimizer,
             epoch=epoch,
             loss_func=loss_func,
-            wandb_logger=wandb_logger,debug=opt['debug'],dice_Train=dice_Train
+            wandb_logger=wandb_logger,dice_Train=dice_Train,opt=opt
         )
         print(
-            "Final training  {}/{}".format(epoch, max_epochs),
+            "Final training  {}/{}".format(epoch, opt.max_epochs),
             "loss: {:.4f}".format(train_loss),
             "time {:.2f}s".format(time.time() - epoch_time),
         )
@@ -334,10 +335,10 @@ def trainer(
                 acc_func=acc_func,
                 model_inferer=model_inferer,
                 post_trans=post_trans,
-                wandb_logger=wandb_logger
+                wandb_logger=wandb_logger,opt=opt
             )
             print(
-                "Final validation stats {}/{}".format(epoch, max_epochs - 1),
+                "Final validation stats {}/{}".format(epoch, opt.max_epochs - 1),
                 ", Dice_Avg:",
                 val_avg_dice,
                 ", time {:.2f}s".format(time.time() - epoch_time),
@@ -367,65 +368,73 @@ def trainer(
 
 if __name__ == "__main__":
 
+# acquire and parse input and output paths
+    parser = argparse.ArgumentParser(description='Command Line Arguments')
+    parser.add_argument("--json_list", type=str, required=False,default='../GIRAFE/Training/training.json',
+                        help="Path of the json file used for training, validation and test")
+    parser.add_argument("--roi", type=int, nargs=3, default= (256, 256) , help='Image Size')
+    parser.add_argument('--batch_size', type=int, default=8, help='# the batchsize')
+    parser.add_argument('--max_epochs', type=int, default=10, help='number of epochs')
+    parser.add_argument('--val_every', type=int, default=2, help='number of epochs to wait to perform validation')
+    parser.add_argument('--lr', type=float, default=2e-4, help='learning rate')
+    parser.add_argument('--seed', type=int, default=1234, help='# of seed for deterministic training')
+    parser.add_argument('--wandb_act', dest='wandb_act',action='store_true', default=False, help='activate wandb')  
+    parser.add_argument('--debug', dest='debug',action='store_true', default=False, help='debugging during validation')  
+    parser.add_argument("--project_name", type=str, required=False,default='Glottis',help="Wandb project name")    
+    parser.add_argument("--model_name", type=str, default='Unet',help="DL model")    
+    parser.add_argument('--channels', nargs='+', default=(16,32,64,128), help='filters for the CNN network')
+    parser.add_argument('--strides', nargs='+', default=(2,2,2), help='strides for the CNN network')
+    parser.add_argument("--nameRun_detail", type=str, required=False,default='Baseline',help="name of the current running")    
+    parser.add_argument('--MoreDA', dest='MoreDA',action='store_true', default=False, help='include extra data augmentation') 
+    parser.add_argument('--device', type=str, required=False,default='cuda',help="cehck cuda or cpu device")  
+
+
+    opt = parser.parse_args()
+
     #######################################################################################################
     ## Set dataset root directory and hyper-parameters
-    os.chdir(os.path.join(os.getcwd(),root_dir))
-    json_list = '../GIRAFE/Training/training.json'
-    roi = (256, 256)  
-    batch_size = 16
-    max_epochs =200
-    val_every = 2
-    lr=2e-4
-    seed=3456
-    wandb_act=False#True
-    debug=True
-    project_name='Glottis'
-    model_name='Unet'
-    channels=(16,32,64,128)
-    strides=(2,2,2)
-    nameRun_detail='Baseline'
-    dir_wandb=os.path.join(os.getcwd(),'wandb')
-    nameRun=model_name+'_'+str(batch_size)+'_'+str(max_epochs)+'_'+str(lr)+'_'+str(roi[0])+'_'+nameRun_detail
+    os.chdir(os.path.join(os.getcwd(),'DL_code'))
+    opt.nameRun=opt.model_name+'_'+str(opt.batch_size)+'_'+str(opt.max_epochs)+'_'+str(opt.lr)+'_'+str(opt.roi[0])+'_'+opt.nameRun_detail
     #######################################################################################################
 
-    set_determinism(seed)
-    np.random.seed(seed)
-    random.seed(seed)
+    set_determinism(opt.seed)
+    np.random.seed(opt.seed)
+    random.seed(opt.seed)
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Create a dictionary from local variables
-    opt = {k: v for k, v in locals().items() if k in ['json_list', 'roi', 'batch_size','max_epochs','val_every','lr','seed','wandb_act','project_name','model_name','nameRun_detail','dir_wandb','nameRun','debug','strides','channels']}
-    opt['device']=device
+    #opt = {k: v for k, v in locals().items() if k in ['json_list', 'roi', 'batch_size','max_epochs','val_every','lr','seed','wandb_act','project_name','model_name','nameRun_detail','dir_wandb','nameRun','debug','strides','channels']}
+    opt.device=device
 
-    if wandb_act:
-        wandb_logger = wandb.init(project=project_name,
+    if opt.wandb_act:
+        wandb_logger = wandb.init(project=opt.project_name,
                                       entity="xamus86",
                                       config=opt,
-                                      name=nameRun,
-                                    dir=dir_wandb)
+                                      name=opt.nameRun,
+                                    dir=opt.dir_wandb)
     else:                                
         wandb_logger=None
 
-    if not os.path.isdir(os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun'])):
-        os.makedirs(os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun']))
+    if not os.path.isdir(os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun)):
+        os.makedirs(os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun))
 
 #######Data Loader##################
-    train_loader, val_loader, test_loader = get_loader(batch_size, json_list, roi) 
+    train_loader, val_loader, test_loader = get_loader(opt.batch_size, opt.json_list, opt.roi,opt.MoreDA) 
 
 #######Model##################
-    if model_name=='Unet':
+    if opt.model_name=='Unet':
         model = UNet(
             spatial_dims=2,
             in_channels=3,
             out_channels=1,
-            channels=channels,
-            strides=strides,
+            channels=opt.channels,
+            strides=opt.strides,
             num_res_units=2,
             norm=Norm.BATCH,
-            ).to(device)
-    elif model_name=='Swinv2':
-        model = SwinV2OneDecoder(img_size=roi).to(device)
+            ).to(opt.device)
+    elif opt.model_name=='Swinv2':
+        model = SwinV2OneDecoder(img_size=opt.roi).to(opt.device)
     else:
         raise ValueError("model not found")
 ########################################################################
@@ -439,8 +448,8 @@ if __name__ == "__main__":
 
     model_inferer = SimpleInferer()
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5) 
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs) 
+    optimizer = torch.optim.AdamW(model.parameters(), lr=opt.lr, weight_decay=1e-5) 
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=opt.max_epochs) 
 
     ## Execute training
     start_epoch = 1
@@ -461,7 +470,7 @@ if __name__ == "__main__":
                             model_inferer=model_inferer,
                             start_epoch=start_epoch,
                             post_trans=post_trans,
-                            val_every=val_every,
+                            val_every=opt.val_every,
                             wandb_logger=wandb_logger,
                             opt=opt
                             )
@@ -480,7 +489,7 @@ if __name__ == "__main__":
     plt.xlabel("epoch")
     plt.plot(trains_epoch, dices_avg, color="green")
     # Save the figure
-    plt.savefig(os.path.join(os.getcwd(),"Results",opt['model_name'],opt['nameRun'],"PlotLoss.pdf"))
+    plt.savefig(os.path.join(os.getcwd(),"Results",opt.model_name,opt.nameRun,"PlotLoss.pdf"))
     #plt.show()
 
     test_data(model, test_loader, dice_Test, model_inferer=model_inferer, post_trans=post_trans,wandb_logger=wandb_logger)
